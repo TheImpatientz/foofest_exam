3b9cd326d296235e8f0c4ae0082e5b3a28d9210b
Julie <julieeriksen09@hotmail.com>
2023-11-30 20:25:15 +0100

    Kreeret newsletter komponent

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,10 @@
+import Newsletter from "@/components/Newsletter";
+
 export default function Home() {
   return (
     <main>
       <p>Marie er strid!</p>
+      <Newsletter></Newsletter>
     </main>
   );
 }
