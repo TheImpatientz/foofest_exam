01b0ede6a0d439ce12c093053c60206c3e4244f6
Marie Kjærside <m.kjaerside@outlook.com>
2023-12-01 15:31:41 +0100

    headertwo 1 del

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Dob from "@/components/Dob";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import HeaderTwo from "@/components/HeaderTwo";
 import Labelinput from "@/components/Labelinput";
 import YourPurchase from "@/components/YourPurchase";
 // import Link from "@/components/Link";
@@ -10,6 +11,7 @@ export default function Home() {
     <>
       <Header></Header>
       <main>
+        <HeaderTwo />
         <YourPurchase campingspot="MUSPELHEIM"></YourPurchase>
       </main>
       <Footer></Footer>
---
+++ src/components/HeaderTwo.jsx
@@ -0,0 +1,8 @@
+export default function HeaderTwo() {
+  return (
+    <h2 className="gap-4 flex relative right-0">
+      <span className="span font-light">FooFest</span>
+      Program
+    </h2>
+  );
+}
