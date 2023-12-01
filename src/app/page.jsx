a83b76050bae62922f91ee2eb9be22355a5f4dc2
Julie <julieeriksen09@hotmail.com>
2023-12-01 11:23:33 +0100

    Tilføjet m til body og main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,15 @@
 import Footer from "@/components/Footer";
-// import Header from "@/components/Header";
+import Header from "@/components/Header";
 // import Link from "@/components/Link";
 
 export default function Home() {
   return (
-    <main>
+    <>
+      <Header></Header>
+      <main>
+        <button className="bg-[var(--accent-color)] p-8">Hey you!</button>
+      </main>
       <Footer></Footer>
-    </main>
+    </>
   );
 }
