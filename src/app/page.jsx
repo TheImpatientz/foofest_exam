ca9fcbd45aa2789e1fb1dc15c8d99e45f2addbe8
Freja Fischer <frej657e@stud.kea.dk>
2023-12-01 15:20:21 +0100

    radio btn virker

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,15 @@
 import Footer from "@/components/Footer";
+import RadioBtn from "@/components/RadioBtn";
 // import Header from "@/components/Header";
-// import Link from "@/components/Link";
+import Link from "@/components/Link";
 
 export default function Home() {
   return (
     <main>
-      <Footer></Footer>
+      <RadioBtn name="camps" id="MUSPELHEIM" status="" />
+      <RadioBtn name="camps" id="Nilfheim" status="" />
+      <RadioBtn name="camps" id="Svartheim" status="" />
+      <RadioBtn name="camps" id="Alfheim" status="soldout" />
     </main>
   );
 }
