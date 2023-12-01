d50e5429cf0c5a3ff3e8a27c506d373f455212a9
Marie Kjærside <m.kjaerside@outlook.com>
2023-12-01 11:22:45 +0100

    knapper i alle afskygninger

---
+++ src/app/PrimaryButton.jsx
@@ -0,0 +1,3 @@
+export default function PrimaryButton() {
+  return <button className="bg-[#c5ef1a] mt-3 font-semibold w-full p-3 px-3  text-[15px] md:p-4 md:px-4 text-[#333131] md:text-[20px]">NEXT</button>;
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,14 @@
+import PrimaryButton from "@/app/PrimaryButton";
+import SecondaryButton from "@/app/SecondaryButton";
+import CheckoutButton from "./CheckoutButton";
+
 export default function Home() {
   return (
     <main>
       <p>Marie er strid!</p>
+      <PrimaryButton />
+      <SecondaryButton />
+      <CheckoutButton />
     </main>
   );
 }
