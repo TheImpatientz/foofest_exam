dc6f83f08b207855f1817e6284bf522167d77b94
Freja Fischer <frej657e@stud.kea.dk>
2023-12-02 16:45:57 +0100

    header fixed

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -13,7 +13,7 @@ export default function Newsletter() {
       <label className="flex mt-1 md:place-self-end" htmlFor="newsletter">
         <input className="border-2 border-[var(--accent-color)] bg-[var(--primary-color)] p-2 pl-6 min-w-[260px] text-[--secondary-color] md:w-[275px]" id="newsletter" name="newsletter" type="text" placeholder="YOUR EMAIL ADRESS" />
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--primary-color)" className="bi bi-arrow-right bg-[var(--accent-color)] h-auto w-[40px] p-2 md:w-[50px]" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+          <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
         </svg>
       </label>
     </div>
