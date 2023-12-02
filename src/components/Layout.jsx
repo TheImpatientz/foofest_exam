7a9fe52344fc38bbd6cab3f870d5f5d5c4cc1bcf
Julie <julieeriksen09@hotmail.com>
2023-12-02 21:28:05 +0100

    Wopsi, layout arbejde i main

---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Header></Header>
+      {children}
+      <Footer></Footer>
+    </div>
+  );
+}
