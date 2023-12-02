927cbdfaaf7dc2eb3215f0ff95930199db5c3daf
Marie Kjærside <m.kjaerside@outlook.com>
2023-12-02 11:50:16 +0100

    headertwo align

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HeaderTwo from "@/components/HeaderTwo";
 import Labelinput from "@/components/Labelinput";
+import SecondaryButton from "@/components/SecondaryButton";
 import YourPurchase from "@/components/YourPurchase";
 // import Link from "@/components/Link";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <Header></Header>
       <main>
         <HeaderTwo />
+        <SecondaryButton />
         <YourPurchase campingspot="MUSPELHEIM"></YourPurchase>
       </main>
       <Footer></Footer>
--- src/components/HeaderTwo.jsx
+++ src/components/HeaderTwo.jsx
@@ -1,8 +1,10 @@
 export default function HeaderTwo() {
   return (
-    <h2 className="gap-4 flex relative right-0">
-      <span className="span font-light">FooFest</span>
-      Program
-    </h2>
+    <div className="flex flex-row-reverse space-x-4 space-x-reverse mb-3">
+      <h2 className="gap-4">
+        <span className="span font-light">FooFest</span>
+        Program
+      </h2>
+    </div>
   );
 }
