5a1255439c62000a33dc91cb52422075135e54ca
Freja Fischer <frej657e@stud.kea.dk>
2023-12-04 16:37:21 +0100

    required added to RadioBtn

--- src/components/RadioBtn.jsx
+++ src/components/RadioBtn.jsx
@@ -12,7 +12,7 @@ export default function RadioBtn({ name, id, status }) {
       <div className={`border border-solid border-[var(--accent-color)] w-full md:w-80 h-28 md:h-44 ${status === "soldout" ? styles.disabled : ""}`}>
         <label htmlFor={id} className="h-full w-full flex flex-col items-center justify-center">
           <div className="w-fit h-fit flex items-center">
-            <input type="radio" className={`${styles.radio} w-5 md:w-6 h-5 md:h-6`} name={name} id={id} disabled={isDisabled} />
+            <input type="radio" className={`${styles.radio} w-5 md:w-6 h-5 md:h-6`} name={name} id={id} disabled={isDisabled} required />
             <p className="text-[var(--secondary-color)] text-xl md:text-4xl ml-4 h-fit">{id}</p>
           </div>
           {status === "soldout" ? <p>Sold out</p> : ""}
