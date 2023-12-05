3974f5c0a319b074a213ee2ae3c642c3e4941c22
Marie Kjærside <m.kjaerside@outlook.com>
2023-12-05 10:02:14 +0100

    border til outline

--- src/components/Labelinput.jsx
+++ src/components/Labelinput.jsx
@@ -4,7 +4,7 @@ export default function Labelinput({ id, inputname, label, placeholder, type, fo
       <label className="text-[var(--secondary-color)] semibold" htmlFor={forId}>
         {label}{" "}
       </label>
-      <input className="bg-[var(--primary-color)] outline-none text-[var(--secondary-color)] p-4 border focus:border-[3px] border-[var(--accent-color)]" id={id} placeholder={placeholder} name={inputname} type={type} />
+      <input className="bg-[var(--primary-color)] outline-none text-[var(--secondary-color)] p-4 outline focus:outline-[3px] outline-[var(--accent-color)]" id={id} placeholder={placeholder} name={inputname} type={type} />
     </div>
   );
 }
