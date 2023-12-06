2b8f92744c266525e7bfe2a2c81c76674217fdea
Freja Fischer <frej657e@stud.kea.dk>
2023-12-06 11:30:50 +0100

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,33 +1,14 @@
-"use client";
+import Image from "next/image";
 
-import Dob from "@/components/Dob";
-import EkstraTicket from "@/components/EkstraTicket";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import HeaderTwo from "@/components/HeaderTwo";
-import Labelinput from "@/components/Labelinput";
+import landingPic from "../img/landingpage_image.jpg";
 import Layout from "@/components/Layout";
-import OurLink from "@/components/OurLink";
-import PrimaryButton from "@/components/PrimaryButton";
-import ProgramContent from "@/components/ProgramContent";
-import ProgramItem from "@/components/ProgramItem";
-import RadioBtn from "@/components/RadioBtn";
-import SecondaryButton from "@/components/SecondaryButton";
-import YourPurchase from "@/components/YourPurchase";
-import Plusminus from "@/components/plusminus";
-
-import { useState } from "react";
 
 export default function Home() {
-  //metode til at få vist "flere sider" ligesom i matasquizzen
-  const [visible, setVisible] = useState(1);
-
-  const testArray = ["test", "test", "test", "test"];
   return (
-    <Layout>
-      <h1>hallo</h1>
-
-      <OurLink text="Klik her til test post siden" href="/postTest" />
+    <Layout className="static">
+      <div className="absolute -inset-x-5 md:-inset-x-6 top-0 w-full h-fit overflow-hidden">
+        <Image src={landingPic} alt="FooFest image" priority className="max-w-none w-auto opacity-50" />
+      </div>
     </Layout>
   );
 }
