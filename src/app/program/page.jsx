462e471977ba7b429c6b8eddbc9df621ea875ded
Freja Fischer <frej657e@stud.kea.dk>
2023-12-07 15:28:13 +0100

    and again

--- src/app/program/page.jsx
+++ src/app/program/page.jsx
@@ -4,13 +4,13 @@ import Schedule from "@/components/Schedule";
 import SecondaryButton from "@/components/SecondaryButton";
 // FETCHER DATA FRA DATABASEN MED SCHEDULE FOR AT TAGE FAT I PROGRAMMET
 export default async function program() {
-  let response = await fetch(`http://localhost:8080/schedule`, {
-    method: "GET",
-  });
-  // SKAL SKIFTES UD MED DENNE
-  // let response = await fetch(`https://plant-flaxen-glove.glitch.me/schedule`, {
+  // let response = await fetch(`http://localhost:8080/schedule`, {
   //   method: "GET",
   // });
+  // SKAL SKIFTES UD MED DENNE
+  let response = await fetch(`https://plant-flaxen-glove.glitch.me/schedule`, {
+    method: "GET",
+  });
 
   const data = await response.json();
   console.log(data);
